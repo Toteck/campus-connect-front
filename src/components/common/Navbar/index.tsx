5793f41b2a57f24ae0628998ffaa70a1040bb23f
mateus <mateusweslley14@gmail.com>
2024-07-02 16:54:37 -0300

    Criação da barra de navegação

---
+++ src/components/common/Navbar/index.tsx
@@ -0,0 +1,56 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { PropsStack } from "@/routes";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const home = require("../../../../assets/icons/main.png");
+const categories = require("../../../../assets/icons/categories.png");
+const search = require("../../../../assets/icons/search-navbar.png");
+const myEvents = require("../../../../assets/icons/my-events.png");
+const person = require("../../../../assets/icons/person.png");
+const Navbar = () => {
+  const navigation = useNavigation<PropsStack>();
+
+  return (
+    <View className="w-full h-20 bg-navigationBar border-r-red-600  absolute bottom-0 flex-row justify-between items-center py-0 px-8">
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      >
+        <Ionicons name="home" size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      >
+        <Ionicons name="menu" size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      >
+        <Ionicons name="search" size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      >
+        <Ionicons name="star" size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      >
+        <Ionicons name="person" size={32} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Navbar;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Header from "@/components/common/Header";
+import Navbar from "@/components/common/Navbar";
 
 const Home = () => {
   return (
     <View className="flex-1 bg-background h-full">
       <Header />
+      <Navbar />
     </View>
   );
 };
