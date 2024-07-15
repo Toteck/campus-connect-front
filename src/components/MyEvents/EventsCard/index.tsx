2327892ca1d3a4a4df79021b8a19eb3a72814b5d
mateus <mateusweslley14@gmail.com>
2024-07-15 16:16:03 -0300

    Adicionando sombra aos cards de MyEvents

--- src/components/MyEvents/EventsCard/index.tsx
+++ src/components/MyEvents/EventsCard/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
 import { EventType } from "@/components/Eventlist";
 
@@ -11,8 +11,9 @@ interface DataProps {
 const EventCard = ({ data }: DataProps) => {
   return (
     <TouchableOpacity
+      style={styles.card}
       activeOpacity={0.85}
-      className="card w-[90%] min-h-fit bg-white rounded-2xl flex-row box-border border border-slate-300"
+      className="card w-[90%] min-h-fit bg-white rounded-2xl p-1 shadow-black shadow flex-row box-border border border-slate-300"
     >
       <Image
         className="bg-slate-400 w-[40%] h-full object-cover rounded-tl-2xl rounded-bl-2xl"
@@ -27,5 +28,17 @@ const EventCard = ({ data }: DataProps) => {
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    elevation: 14,
+  },
+});
 
 export default EventCard;
