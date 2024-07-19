9ff7aa4fa8d1b046785156f7c8cef1a78232e38f
mateus <mateusweslley14@gmail.com>
2024-07-19 14:45:37 -0300

    Finalização da tela de resultados de pesquisa

--- src/screens/ResultSearch/index.tsx
+++ src/screens/ResultSearch/index.tsx
@@ -1,13 +1,35 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DefaultTitle from "@/components/common/DefaultTitle";
+import Eventlist from "@/components/Eventlist";
+import Navbar from "@/components/common/Navbar";
+import { NoResult } from "../Search/style";
+import Loading from "@/components/Loading";
 
 const ResultSearch = () => {
+  const [data, setData] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(false);
   return (
-    <SafeAreaView>
-      <DefaultTitle title="Resultados da busca" />
-    </SafeAreaView>
+    <View className="flex-1 bg-background">
+      <SafeAreaView>
+        <DefaultTitle title="Resultados da busca" />
+      </SafeAreaView>
+      {!loading ? (
+        data.length <= 0 ? (
+          <NoResult>
+            Sua pesquisa não trouxe nenhum{"\n"}evento, tente novamente mais
+            tarde!
+          </NoResult>
+        ) : (
+          <Eventlist />
+        )
+      ) : (
+        <Loading />
+      )}
+
+      <Navbar />
+    </View>
   );
 };
 
